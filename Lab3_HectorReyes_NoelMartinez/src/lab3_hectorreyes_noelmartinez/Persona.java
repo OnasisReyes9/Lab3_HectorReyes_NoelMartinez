65a07b0a691ac720d3adc34bb41c04045c603b33
OnasisReyes9 <onasis660@unitec.com>
2020-08-07 13:54:19 -0600

    Creacion clases importantes para trabajar en pareja

---
+++ Lab3_HectorReyes_NoelMartinez/src/lab3_hectorreyes_noelmartinez/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_hectorreyes_noelmartinez;
+
+/**
+ *
+ * @author Onasis Reyes
+ */
+public class Persona {
+    
+    private String nombre;
+    private Posicion posiciones;
+    private long identificacion;
+    private EstadoRegistro estadoRegistro;
+    
+}
---
+++ Lab3_HectorReyes_NoelMartinez/src/lab3_hectorreyes_noelmartinez/Piso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_hectorreyes_noelmartinez;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Onasis Reyes
+ */
+public class Piso {
+    
+    private Administrador admin;
+    private Ranker evaluadores;
+    private Ranker director;
+    private ArrayList <Persona> personas = new ArrayList();
+    
+}
