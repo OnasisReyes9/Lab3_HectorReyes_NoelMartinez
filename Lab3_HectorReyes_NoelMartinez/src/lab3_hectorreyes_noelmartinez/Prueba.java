6281c6d6837de4d8c2e64f4a267f04f1828d461e
Noel-coder <noelgg_23@outlook.es>
2020-08-07 15:29:38 -0600

    Clases finales (noel)

--- Lab3_HectorReyes_NoelMartinez/src/lab3_hectorreyes_noelmartinez/Prueba.java
+++ Lab3_HectorReyes_NoelMartinez/src/lab3_hectorreyes_noelmartinez/Prueba.java
@@ -5,10 +5,62 @@
  */
 package lab3_hectorreyes_noelmartinez;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author noelg
  */
 public class Prueba {
-    private String nombre; 
+    private String nombrePrueba;
+    private Persona evaluadorPrueba;
+    private ArrayList<Normal>equipo = new ArrayList();
+    private String estado;
+
+    public Prueba() {
+    }
+
+    public Prueba(String nombrePrueba, Persona evaluadorPrueba, String estado) {
+        this.nombrePrueba = nombrePrueba;
+        this.evaluadorPrueba = evaluadorPrueba;
+        this.estado = estado;
+    }
+
+    public String getNombrePrueba() {
+        return nombrePrueba;
+    }
+
+    public void setNombrePrueba(String nombrePrueba) {
+        this.nombrePrueba = nombrePrueba;
+    }
+
+    public Persona getEvaluadorPrueba() {
+        return evaluadorPrueba;
+    }
+
+    public void setEvaluadorPrueba(Persona evaluadorPrueba) {
+        this.evaluadorPrueba = evaluadorPrueba;
+    }
+
+    public ArrayList<Normal> getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(ArrayList<Normal> equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Prueba{" + "nombrePrueba=" + nombrePrueba + ", evaluadorPrueba=" + evaluadorPrueba + ", equipo=" + equipo + ", estado=" + estado + '}';
+    }
+    
 }
